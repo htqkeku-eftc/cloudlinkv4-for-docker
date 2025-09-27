9a54388271f736483631418321733ad616b626c3
htqkeku-eftc <3872870194@qq.com>
2025-09-27 12:22:47 +0800

    Update server_example.py

--- python/server_example.py
+++ python/server_example.py
@@ -89,5 +89,5 @@ if __name__ == "__main__":
     # server.enable_ssl(certfile="cert.pem", keyfile="privkey.pem")
     
     # Start the server
-    #server.run(ip="0.0.0.0", port=3000)
-     server.run(ip="127.0.0.1", port=3000)
+    # server.run(ip="0.0.0.0", port=3000)
+    server.run(ip="127.0.0.1", port=3000)
